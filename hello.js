71b1f385ae60ccb88ead899839373e31a992262c
www-data <ark@fastmail.fm>
2015-12-30 22:49:39 +0000

    Final commit :sunglasses:

--- hello.js
+++ hello.js
@@ -1,1 +1,1 @@
-9b730252-af47-11e5-8d62-f23c9173afb8
+function NewObject(prefix) { var count=0; this.SayHello=function(msg) { count++; alert(prefix+msg); } this.GetCount=function() { return count; } } var obj=new NewObject("Message : "); obj.SayHello("You are welcome.");
